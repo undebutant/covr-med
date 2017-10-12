85f6aca273346c1ab1dd350983c5eb35797ba2e1
Samihael <32427349+Samihael@users.noreply.github.com>
2017-10-12 16:20:34 +0200

    Selection et déplacement des objets (souris et clavier)

---
+++ Assets/Scripts/Engines.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engines : MonoBehaviour
+{
+    private Vector3 speed;
+
+    public Vector3 Speed
+    {
+        get
+        {
+            return this.speed;
+        }
+        set
+        {
+            this.speed = value;
+        }
+    }
+
+
+    void Update()
+    {
+
+        transform.Translate(speed * this.GetComponent<ObjectAvatar>().MaxSpeed * Time.deltaTime);
+
+
+    }
+}
---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseDrag : MonoBehaviour
+{
+
+    /*  float distance = 10;
+
+      void OnMouseDrag()
+      {
+          Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+          Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+          transform.position = objectPosition;
+      }*/
+
+    Vector3 offset;
+    Vector3 screenPoint;
+    void OnMouseDown()
+    {
+
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+
+}
