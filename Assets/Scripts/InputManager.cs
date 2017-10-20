a80b7f804714d6438290d19cfc665e2fc81dcc68
LBT76240 <trailin_88_@hotmail.fr>
2017-10-21 01:00:08 +0200

    ajout des commentaires sur l'input manager

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,16 +2,19 @@
 using System.Collections;
 
 public class InputManager : MonoBehaviour {
-
+    //Position et Rigidbody de la caméra
     public Transform cameraTransform;
     public Rigidbody cameraRigidbody;
 
+    //Variables de sensibilité
     public float sensitivityX;
     public float sensitivityY;
+    //Variable d'angle max
     public float minimumX = -360F;
     public float maximumX = 360F;
     public float minimumY = -60F;
     public float maximumY = 60F;
+    //Vitesse du déplacement
     public float speed;
 
     float rotationY = 0F;
@@ -23,13 +26,13 @@ public class InputManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         
-
+        //Calcul des angles de déplacement en fonction de la position de la souris
         float rotationX = cameraTransform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 
         rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
         rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
 
-
+        //Calcule des déplacements en hauteur
         float yMove = 0;
         if (Input.GetButton("Up")) {
             
@@ -40,10 +43,14 @@ public class InputManager : MonoBehaviour {
             yMove -= 1;
         }
 
+        //Calcule des déplacements dans le plan
+        float xMove = Input.GetAxis("Horizontal")  * Mathf.Cos(cameraTransform.rotation.eulerAngles.y * Mathf.PI / 180) + Input.GetAxis("Vertical")  * Mathf.Sin(cameraTransform.rotation.eulerAngles.y * Mathf.PI / 180);
+        float zMove = -Input.GetAxis("Horizontal") * Mathf.Sin(cameraTransform.rotation.eulerAngles.y * Mathf.PI / 180) + Input.GetAxis("Vertical") * Mathf.Cos(cameraTransform.rotation.eulerAngles.y * Mathf.PI / 180);
+
+        //Rotation
         rotateCamera(new Vector3(-rotationY, rotationX, 0));
-        
-        moveCamera(new Vector3(Input.GetAxis("Horizontal") * speed* Mathf.Cos(cameraTransform.rotation.eulerAngles.y * Mathf.PI / 180) + Input.GetAxis("Vertical") * speed * Mathf.Sin(cameraTransform.rotation.eulerAngles.y * Mathf.PI / 180), 1* yMove*speed, 
-            -Input.GetAxis("Horizontal") * speed* Mathf.Sin(cameraTransform.rotation.eulerAngles.y * Mathf.PI / 180) + Input.GetAxis("Vertical") * speed * Mathf.Cos(cameraTransform.rotation.eulerAngles.y * Mathf.PI / 180)));
+        //Déplacement
+        moveCamera(new Vector3(xMove, yMove, zMove)*speed);
         
 
 
@@ -54,14 +61,16 @@ public class InputManager : MonoBehaviour {
     
 
     void rotateCamera(Vector3 rotation) {
+        //Anulation de la vélocité sur la rotation
         cameraRigidbody.angularVelocity = new Vector3(0,0,0);
+
         cameraTransform.localEulerAngles = rotation;
     }
 
 
     void moveCamera(Vector3 move) {
         cameraRigidbody.velocity = move;
-        //cameraTransform.Translate(move);
+        
     }
 
 
