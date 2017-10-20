b253f80762e32069cbb940685deb1157e95f3bc8
LBT76240 <trailin_88_@hotmail.fr>
2017-10-20 21:24:49 +0200

    deplacement clavier

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -11,6 +11,7 @@ public class InputManager : MonoBehaviour {
     public float maximumX = 360F;
     public float minimumY = -60F;
     public float maximumY = 60F;
+    public float speed;
 
     float rotationY = 0F;
     // Use this for initialization
@@ -20,23 +21,7 @@ public class InputManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        /*
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-
         
-        if (mouseY == 0) {
-            cameraTransform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-        } else if (mouseX == 0) {
-
-        } else {
-            float rotationX = cameraTransform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-
-            cameraTransform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-        }*/
 
         float rotationX = cameraTransform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 
@@ -44,5 +29,8 @@ public class InputManager : MonoBehaviour {
         rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
 
         cameraTransform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+        cameraTransform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*speed, 0, Input.GetAxis("Vertical")*Time.deltaTime*speed);
+
     }
 }
