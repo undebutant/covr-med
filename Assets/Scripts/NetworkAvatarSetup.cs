518d24408d7ec31794d56b1b44975ec7c2c936a4
Guillaume Gonthier <guillaume.gonthier@telecom-sudparis.eu>
2017-11-30 01:29:06 +0100

    Working moving avatars using Unity's network transform manager

---
+++ Assets/Scripts/NetworkAvatarSetup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+/// <summary>
+///     Setup script used at the creation of the local player avatar.
+///     Heritates from NetworkBehaviour
+/// </summary>
+public class NetworkAvatarSetup : NetworkBehaviour {
+
+    [SerializeField]
+    [Tooltip("The camera of the player avatar prefab, to set active on creation of the avatar")]
+    GameObject playerCamera;
+
+
+    void Start() {
+        if(isLocalPlayer) {
+            // Enabling the avatar's camera
+            playerCamera.SetActive(true);
+
+            // Disabling the global camera of the scene
+            GameObject.FindWithTag("SceneCamera").SetActive(false);
+        }
+    }
+}
