17dadf193c76a43e6107b4a45a20748576cc1cd4
mehdi <mehdi.khadir@catenate.com>
2018-01-26 17:04:52 +0100

    Added a main menu manager to display the text on the main menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+    // ========== Set the texts displayed on the main menu ============
+
+    /// <summary>
+    ///     Sets the role displayed on the button
+    /// </summary>
+    /// <param name="role">Role to display</param>
+    public void SetRoleButtonDisplay(string role) {
+        //TODO
+    }
+
+    /// <summary>
+    ///     Sets the 'display device' displayed on the button
+    /// </summary>
+    /// <param name="role">Display device's name to display on the button in the menu</param>
+    public void SetDisplayDeviceButtonDisplay(string role) {
+
+    }
+
+    /// <summary>
+    ///     Sets the input device on the button
+    /// </summary>
+    /// <param name="inputDevice">Input device to display</param>
+    public void SetInputDeviceButtonDisplay(string inputDevice) {
+
+    }
+
+    /// <summary>
+    ///     Sets the host server IP to display on the button
+    /// </summary>
+    /// <param name="hostIp">Host server IP to display on the button in the main menu</param>
+    public void SetHostIPDisplay(string hostIp) {
+
+    }
+
+    /// <summary>
+    ///     Sets the host server's port to display on the button in the main menu
+    /// </summary>
+    /// <param name="hostPort">host servers's port to display on the button in the main menu</param>
+    public void SetHostPortDisplay(string hostPort) {
+
+    }
+}
