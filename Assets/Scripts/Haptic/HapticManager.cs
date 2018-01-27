dc05a8e4015965bdc97bb3ddcf0f0e657dcbe2e7
Samihael <32427349+Samihael@users.noreply.github.com>
2018-01-27 13:24:28 +0100

    Déplacement / rotation de la main avec le bras haptique

--- Assets/Scripts/Haptic/HapticManager.cs
+++ Assets/Scripts/Haptic/HapticManager.cs
@@ -16,45 +16,75 @@ public class HapticManager : MonoBehaviour {
     private static string _tag = ".::. HapticManager .::. ";
 
     private SimplePhantomUnity Phantom = null;
-    //c'était private on verra apres
 
-    public Vector3 HandPosition = Vector3.zero;
-    public Quaternion HandRotation = Quaternion.identity;
+    Vector3 handPosition;
+    Quaternion handRotation;
+
+    
+    Vector3 offset;
+
+    Quaternion offsetRotation;
+
+    [SerializeField]
+    GameObject hand;
+
+    [SerializeField]
+    GameObject player;
+
+    //Variaple to reduce plage of Haptic movement
+    [SerializeField]
+    int downScale = 150;
+
+    public  Vector3 HandPosition
+    {
+        get
+        {
+            return handPosition;
+        }
+    }
 
 
+    public Quaternion HandRotation
+    {
+        get
+        {
+            return handRotation;
+        }
+    }
+
+
+    // Initializes communication with Phantom device
     public bool InitHaptics()
     {
-
         // Initializes variables
         Init();
 
         Phantom = new SimplePhantomUnity();    
         Phantom.AddSchedule(PhantomUpdate,Hd.Priority.HD_RENDER_EFFECT_FORCE_PRIORITY);
-
-        //while (!Phantom.IsAvailable) ;
-
         Phantom.Start();
 
         return true;
-
     }
 
+
+    // Initialization of the manager
     private void Init()
     {
         // Initialization of hand position and orientation
-        HandPosition = Vector3.zero;
-        HandRotation = Quaternion.identity;
-
+        handPosition = Vector3.zero;
+        handRotation = Quaternion.identity;
     }
 
 
-    /// Process when disabling the application
+    // Process when disabling the application
     private void OnDisable()
     {
         Debug.Log(_tag + "Haptic go out on disable");
         StopHaptics();
     }
 
+
+    // Stops device communication
     public bool StopHaptics()
     {
         if (Phantom == null || !Phantom.IsRunning)
@@ -68,23 +98,49 @@ public class HapticManager : MonoBehaviour {
 
         return true;
     }
+
     // Use this for initialization
     void Start () {
-        // ...
+        offset = hand.transform.localPosition;
+        offsetRotation = hand.transform.localRotation;
+        Debug.LogError(offsetRotation);
+      
         InitHaptics();
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
         if (Phantom != null) 
-        Debug.Log(HandPosition);
+        Debug.Log(handPosition);
 
     }
-     private bool PhantomUpdate()
+
+
+    // Function to be executed asynchronously from the haptic device
+    // Responsable of all the haptic force feedback during simulation
+    private bool PhantomUpdate()
     {
-        HandPosition = Phantom.GetPosition();
-        HandRotation = Phantom.GetRotation();
+
+        Vector3 haptPosition = Phantom.GetPosition() / downScale;
+        //adapting haptic axes to unity axes 
+        handPosition.x = - haptPosition.z;
+        handPosition.z = haptPosition.x;
+        handPosition.y = haptPosition.y;
+        handPosition = handPosition + offset;
+
+        Quaternion haptRotation = Phantom.GetRotation();
+        //get euler vector from quaternion to be aple to apply changes
+        Vector3 eulerVector = haptRotation.eulerAngles;
+
+        float temp = eulerVector.x;
+        eulerVector.x = -eulerVector.z;
+        eulerVector.z = temp;
+
+        haptRotation = Quaternion.Euler(eulerVector.x, eulerVector.y, eulerVector.z);
+        handRotation = haptRotation * offsetRotation;
 
         return true;
     }
