bd8246a74987558c98efbf38e0d08c7770ea1423
mehdi <mehdi.khadir@catenate.com>
2018-01-29 18:08:32 +0100

    Commented the part related to MiddleVR in the WandSelection script not to block everyone

--- Assets/Scripts/WandSelection.cs
+++ Assets/Scripts/WandSelection.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MiddleVR_Unity3D;
+//using MiddleVR_Unity3D;
 
 public class WandSelection : MonoBehaviour {
 
@@ -27,34 +27,34 @@ public class WandSelection : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-        RaycastHit hit;
-        if (MiddleVR.VRDeviceMgr != null) {
-            // Getting state of primary wand button
-            bool wandButtonPressed0 = MiddleVR.VRDeviceMgr.IsWandButtonPressed(0);
+        //Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        //RaycastHit hit;
+        //if (MiddleVR.VRDeviceMgr != null) {
+        //    // Getting state of primary wand button
+        //    bool wandButtonPressed0 = MiddleVR.VRDeviceMgr.IsWandButtonPressed(0);
 
-            // Getting toggled state of primary wand button
-            // bool wandButtonToggled0 = MiddleVR.VRDeviceMgr.IsWandButtonToggled(0);
+        //    // Getting toggled state of primary wand button
+        //    // bool wandButtonToggled0 = MiddleVR.VRDeviceMgr.IsWandButtonToggled(0);
 
-            if (Physics.Raycast(transform.position, fwd, out hit)) {
-                if(hit.collider.gameObject.layer == selectableObjectsLayer) {
-                    // Set the color of the wand's ray
-                    GetComponent<VRWand>().SetRayColor(GetComponent<VRRaySelection>().HoverColor);
-                    if (wandButtonPressed0) {
-                        // Here, call the drag and drop
+        //    if (Physics.Raycast(transform.position, fwd, out hit)) {
+        //        if(hit.collider.gameObject.layer == selectableObjectsLayer) {
+        //            // Set the color of the wand's ray
+        //            GetComponent<VRWand>().SetRayColor(GetComponent<VRRaySelection>().HoverColor);
+        //            if (wandButtonPressed0) {
+        //                // Here, call the drag and drop
                         
-                    }
-                } else if (hit.collider.gameObject.layer == buttonObjectsLayer) {
-                    // Set the color of the wand's ray
-                    GetComponent<VRWand>().SetRayColor(GetComponent<VRRaySelection>().HoverColor);
-                    // Click on the button only once
-                    if (wandButtonPressed0 && !clicked) {
-                        Debug.Log("Button clicked");
-                        clicked = true;
-                        mainMenuManager.OnHitButton(hit.collider.gameObject);
-                    }
-                }
-            }
-        }
+        //            }
+        //        } else if (hit.collider.gameObject.layer == buttonObjectsLayer) {
+        //            // Set the color of the wand's ray
+        //            GetComponent<VRWand>().SetRayColor(GetComponent<VRRaySelection>().HoverColor);
+        //            // Click on the button only once
+        //            if (wandButtonPressed0 && !clicked) {
+        //                Debug.Log("Button clicked");
+        //                clicked = true;
+        //                mainMenuManager.OnHitButton(hit.collider.gameObject);
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
