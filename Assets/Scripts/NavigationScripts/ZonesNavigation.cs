46cdc4ec0ef85af1379fa2b20eaf5ef2525d8efa
Mehdi Khadir <mehdi.khadir@ensiie.fr>
2018-02-05 13:02:24 +0100

    Created a zone navigation script that allows the user to move the local prefab player to a specific zone

---
+++ Assets/Scripts/NavigationScripts/ZonesNavigation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ZonesNavigation : NetworkBehaviour {
+
+    GameObject prefabPlayer;
+
+    void Start() {
+        prefabPlayer = this.gameObject;
+    }
+
+    /// <summary>
+    ///     Sets the new position of the avatar. Only horizontal translations are operated
+    /// </summary>
+    /// <param name="zone">The zone to head to</param>
+    public void SetDestination(GameObject zone){
+        if (isLocalPlayer) {
+            prefabPlayer.transform.position = new Vector3(zone.transform.position.x, prefabPlayer.transform.position.y, zone.transform.position.z);
+        }
+    }
+}
