2596b82b769890777ce53f283649ba877508a429
Mehdi Khadir <mehdi.khadir@ensiie.fr>
2018-02-09 16:44:23 +0100

    Full scenario implemented. Only the surgeon can see the instructions

--- Assets/Scripts/Scenario/Scenario.cs
+++ Assets/Scripts/Scenario/Scenario.cs
@@ -9,6 +9,11 @@ using System.IO;
 public struct Step {
     public GameObject zone;
     public GameObject selectableObject;
+
+    [Tooltip("If true, the object must be close to achieve the step")]
+    public bool isCloseCondition;
+
+    [Tooltip("Instruction to show while the step is not achieved")]
     public String instruction;
 }
 
@@ -19,23 +24,38 @@ public class Scenario : MonoBehaviour {
     int currentStepIndex;
 
     //  Displayer
-
     [SerializeField]
+    [Tooltip("The text component that displays the information for the surgeon")]
     Text displayer;
 
+    // The config for the local instance
+    ConfigInitializer config;
+
+    bool IsClose() {
+        Debug.Log(steps[currentStepIndex].zone.name);
+        return Vector3.Distance(steps[currentStepIndex].selectableObject.transform.position, steps[currentStepIndex].zone.transform.position) < 0.1;
+    }
 
 
     // Use this for initialization
     void Start() {
         currentStepIndex = 0;
-        // Show the first instructions
-        displayer.text = steps[currentStepIndex].instruction;
+        config = GameObject.FindObjectOfType<ConfigInitializer>();
+        // Only surgeons can see the instructions
+        if (config.GetPlayerRole() == PlayerRole.Surgeon) {
+            // Show the first instructions
+            displayer.text = steps[currentStepIndex].instruction;
+        } else {
+            displayer.text = "Please follow the surgeon's vocal instructions";
+        }
     }
 
     void Update() {
-        if (Vector3.Distance(steps[currentStepIndex].selectableObject.transform.position, steps[currentStepIndex].zone.transform.position) < 0.1) {
-            currentStepIndex++;
-            displayer.text = steps[currentStepIndex].instruction;
+        if (config.GetPlayerRole() == PlayerRole.Surgeon) {
+            if (steps[currentStepIndex].isCloseCondition == IsClose()) {
+                currentStepIndex++;
+                displayer.text = steps[currentStepIndex].instruction;
+            }
         }
     }
 }
