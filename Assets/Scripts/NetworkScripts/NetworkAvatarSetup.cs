abeed488aa98080dc8f47bea768529ce440d7da1
Guillaume Gonthier <guillaume.gonthier@telecom-sudparis.eu>
2018-02-10 05:52:45 +0100

    Disabling some scripts and collider on online player avatar, using networkavatarsetup

--- Assets/Scripts/NetworkScripts/NetworkAvatarSetup.cs
+++ Assets/Scripts/NetworkScripts/NetworkAvatarSetup.cs
@@ -27,13 +27,19 @@ public class NetworkAvatarSetup : NetworkBehaviour {
     /// </summary>
     PlayerRole playerRole;
 
+    // Camera GameObject
     [SerializeField]
     [Tooltip("The camera of the player avatar prefab, to set active on creation of the avatar")]
     GameObject playerCamera;
 
+    // PlayerAvatar GameObject
     [SerializeField]
     [Tooltip("The avatar rendering of the player avatar prefab, to set active on creation of the avatar")]
     GameObject playerAvatar;
+
+    [SerializeField]
+    [Tooltip("The avatar hand script of the player avatar prefab, to set active on creation of the avatar")]
+    Hand playerHandScript;
 
     [SerializeField]
     [Tooltip("The collider of the player avatar prefab, to set active on creation of the avatar")]
@@ -44,8 +50,25 @@ public class NetworkAvatarSetup : NetworkBehaviour {
     InputManager playerInputManager;
 
     [SerializeField]
+    [Tooltip("The zone navigation manager of the player avatar prefab, to set active on creation of the avatar")]
+    ZonesNavigation playerZoneNavigation;
+
+    [SerializeField]
     [Tooltip("The haptic manager of the player avatar prefab, to set active on creation of the avatar if haptic input selected")]
     HapticManager hapticManager;
+
+    // Hand GameObject
+    [SerializeField]
+    [Tooltip("The collider of the player avatar hand prefab, to set active on creation of the avatar")]
+    Collider playerHandCollider;
+
+    [SerializeField]
+    [Tooltip("The object drag script of the player avatar hand prefab, to set active on creation of the avatar")]
+    ObjectDrag playerHandObjectDrag;
+
+    [SerializeField]
+    [Tooltip("The Hand Collider script of the player avatar hand prefab, to set active on creation of the avatar")]
+    HandCollider playerHandColliderScript;
 
     public GameObject GetPlayerCamera() {
         return playerCamera;
@@ -60,14 +83,11 @@ public class NetworkAvatarSetup : NetworkBehaviour {
         playerRole = configInitializer.GetPlayerRole();
 
         if (isLocalPlayer) {
-            // Enabling the avatar's camera
+            // Enabling the avatar's camera GameObject
             playerCamera.SetActive(true);
 
-            // Enabling the player's collider if we are not in the cave
-            if (configInitializer.GetDisplayDevice() != DisplayDevice.Cave) {
-                playerCollider.enabled = true;
-            }
-            
+            // Enabling the player's collider
+            playerCollider.enabled = true;
 
             // Enabling the player's input manager
             playerInputManager.enabled = true;
@@ -75,42 +95,57 @@ public class NetworkAvatarSetup : NetworkBehaviour {
             // Disabling the player's avatar
             playerAvatar.SetActive(false);
 
+            // Enabling the hand management script
+            playerHandScript.enabled = true;
+
+            // Enabling the hand collider
+            playerHandCollider.enabled = true;
+
+            // Enabling the drag and drop for the hand
+            playerHandObjectDrag.enabled = true;
+
+            // Enabling the hand onColliding methods
+            playerHandColliderScript.enabled = true;
+
             // Enable haptic manager if haptic input selected
             if (configInitializer.GetInputDevice() == InputDevice.Haptic) {
                 hapticManager.enabled = true;
             }
 
-            if (isLocalPlayer) {
-                if (playerRole == PlayerRole.Surgeon) {
-                    // Positionning the surgeon
-                    this.transform.position = surgeonSpawner.transform.position;
-                    this.transform.rotation = surgeonSpawner.transform.rotation;
-
-                    // Playing the sit animation for the surgeon avatar
-                    playerAvatar.GetComponent<Animation>().Play("M_Sit_Idle_2");
-                } else {
-                    // Positionning the nurse
-                    this.transform.position = nurseSpawner.transform.position;
-                    this.transform.rotation = nurseSpawner.transform.rotation;
-                }
-            } else {
-                if (playerRole == PlayerRole.Surgeon)
-                {
-                    // Positionning the nurse
-                    this.transform.position = nurseSpawner.transform.position;
-                    this.transform.rotation = nurseSpawner.transform.rotation;
-                } else {
-                    // Positionning the surgeon
-                    this.transform.position = surgeonSpawner.transform.position;
-                    this.transform.rotation = surgeonSpawner.transform.rotation;
-
-                    // Playing the sit animation for the surgeon avatar
-                    playerAvatar.GetComponent<Animation>().Play("M_Sit_Idle_2");
-                }
+            // Enable zone navigation if we have a laser pointer
+            if(configInitializer.GetInputDevice() == InputDevice.Remote) {
+                playerZoneNavigation.enabled = true;
             }
 
-        // Disabling the global camera of the scene
+            if (playerRole == PlayerRole.Surgeon) {
+                // Positionning the surgeon
+                this.transform.position = surgeonSpawner.transform.position;
+                this.transform.rotation = surgeonSpawner.transform.rotation;
+
+                // Playing the sit animation for the surgeon avatar
+                playerAvatar.GetComponent<Animation>().Play("M_Sit_Idle_2");
+            } else {
+                // Positionning the nurse
+                this.transform.position = nurseSpawner.transform.position;
+                this.transform.rotation = nurseSpawner.transform.rotation;
+            }
+
+            // Disabling the global camera of the scene
             GameObject.FindWithTag("SceneCamera").SetActive(false);
+        } else {
+            // It is not a local player avatar
+            if (playerRole == PlayerRole.Surgeon) {
+                // Positionning the nurse
+                this.transform.position = nurseSpawner.transform.position;
+                this.transform.rotation = nurseSpawner.transform.rotation;
+            } else {
+                // Positionning the surgeon
+                this.transform.position = surgeonSpawner.transform.position;
+                this.transform.rotation = surgeonSpawner.transform.rotation;
+
+                // Playing the sit animation for the surgeon avatar
+                playerAvatar.GetComponent<Animation>().Play("M_Sit_Idle_2");
+            }
         }
     }
 
